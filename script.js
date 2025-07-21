7ec6354179ea91adc9cce72fda19f6f738281834
Redwan <rahatofc912@gmail.com>
2025-07-21 20:26:33 +0600

    Updated

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ const menuToggle = document.getElementById('menuToggle');
 const navLinks = document.getElementById('navLinks');
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
-});
+}); 
